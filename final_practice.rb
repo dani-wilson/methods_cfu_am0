5175da3c90009375d72f2b4f2df0784f3c66098e
Dani Wilson <danirae.powell@gmail.com>
2023-03-30 20:52:24 -0600

    Complete question 3

--- final_practice.rb
+++ final_practice.rb
@@ -23,12 +23,16 @@ puts custom_greeting("Jonathan")
 
 
 
-# 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, and returns a sentence with the full name.
+# 3: Write a method named greet_person that takes in 3 strings, a first, middle, and last name, 
+#and returns a sentence with the full name.
 
-# What is the return value of your method?
-# How many arguments did you pass your method?
+# What is the return value of your method? I'm very hapy to meet you, Dani Rae Wilson!
+# How many arguments did you pass your method? 3
 # What data type was your argument(s)?
-
+def greet_person(first_name, middle_name, last_name)
+    "I'm very happy to meet you, #{first_name} #{middle_name} #{last_name}!"
+end
+puts greet_person("Dani", "Rae", "Wilson")
 
 # 4: Write a method named square that takes in one integer, and returns the square of that integer.
 # Bonus: Print a sentence that interpolates the return value of your square method.
